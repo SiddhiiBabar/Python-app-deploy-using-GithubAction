2589928c89b26e07cca1d7fcea420b4f812bf744
SiddhiiBabar <siddhibabar93@gmail.com>
2026-02-25 19:36:16 +0530

    Edited deploy.yml file

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ html_content = """
   <title>Python App Deployment</title>
 </head>
 <body>
-  <h1>🚀 Python App Successfully Deployed using GitHub Actions!</h1>
-  <p>CI/CD Pipeline working perfectly.</p>
+  <h1> Python App Successfully Deployed using GitHub Actions!</h1> 
 </body>
 </html>
 """
